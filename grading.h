ff7944ce75a9af6acd88ccfe177baf48bd4a10aa
Karan Kushwaha <karankushwaha18210@gmail.com>
2025-11-30 19:26:27 +0530

    Add files via upload

---
+++ grading.h
@@ -0,0 +1,10 @@
+ #ifndef GRADING_H
+#define GRADING_H
+
+#include "user.h"
+
+float calcPercentage(int score, int total);
+char calcGrade(float percent);
+void displayResult(struct Student s, int score, float percent, char grade);
+
+#endif // GRADING_H
